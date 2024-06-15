3c6c056248372c0729f216427dad427c1d0e30ac
Viral Vaghela <viralvaghela8577@gmail.com>
2024-06-16 00:55:26 +0530

    bug - fix

--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-const navigate = useNavigate();
 const Banner = () => {
+  const navigate = useNavigate();
   return (
     <div className="homie-banner">
       <div className="text-center labelText position-absolute">
