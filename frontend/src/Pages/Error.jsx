45ac862f1be133ab5c95d79f152dd29b6995f075
Viral Vaghela <viralvaghela8577@gmail.com>
2024-06-28 18:27:13 +0530

    Revert "Merge branch 'main' of https://github.com/codebyviral/Homieflix"
    This reverts commit 6cabf1c4249425e3782c31298151d6aa88bb3c76, reversing
    changes made to f87d6f90c175ee0e8d85bb11e0879752680b5cf1.

--- frontend/src/Pages/Error.jsx
+++ frontend/src/Pages/Error.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar } from "../Components/CompIndex";
 import ErrorImg from "../assets/error.png";
-
 const Error = () => {
   return (
     <>
       <Navbar />
-      <div className="d-flex flex-column justify-content-center align-items-center min-vh-100 bg-light">
+      <div className="position-absolute top-50 start-50 translate-middle">
         <div className="text-center">
-          <img className="errorImg img-fluid" src={ErrorImg} alt="Error" />
+          <img className="errorImg" src={ErrorImg} alt="" />
         </div>
-        <div className="text-center mt-4">
-          <h1 className="display-1 fw-bold text-primary">404</h1>
-          <h2 className="mt-3">Sorry! Page not Found</h2>
+        <div className="text-center  fs-1">
+          404
+          <h2 className="">Sorry! Page not Found</h2>
         </div>
-        <div className="text-center mt-4">
+        <div className="text-white text-center">
           <Link to="/">
-            <button className="btn btn-primary btn-lg rounded-pill">
-              Return to Home
+            <button className="btn mt-5 btn-light rounded-pill">
+              return to home
             </button>
           </Link>
         </div>
